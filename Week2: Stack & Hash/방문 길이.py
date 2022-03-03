1fdac3d973ba62eb5f70c0a6100491565b3c6314
jungmin <wjdals05070@gmail.com>
2022-03-03 12:01:12 +0900

    prgrms-ct-py8

--- Week2: Stack & Hash/방문 길이.py
+++ Week2: Stack & Hash/방문 길이.py
@@ -1,27 +1,6 @@
-from collections import deque
-
 def solution(dirs):
-    dirs = deque(dirs)
-    start = [0,0]
-    stack =[]
-    for i in range(0, len(dirs)):
-        temp = [0, 0]
 
-        key = dirs.popleft()
-        print(key)
-        if key == 'U':
-            start[1] += 1
-            temp = start
-            stack.append(temp)
-            temp=[0,0]
-        elif key == 'D':
-            start[1] -= 1
-            temp = start
-            stack.append(temp)
-            temp = [0, 0]
-        print(stack)
+    for i in len(dirs):
 
-    return len(stack)
 
-dirs = "UUUDD"
-print(solution(dirs))
+    return answer
--- Week2: Stack & Hash/사전순 부분문자열.py
+++ Week2: Stack & Hash/사전순 부분문자열.py
@@ -1,4 +1,16 @@
 
 def solution(s):
-    s=list(s)
-    
+    s = list(s)
+    print(s)
+    stack = []
+    # x y b  -> yb
+    for i in s:
+        while stack and stack[-1] < i:
+            stack.pop()
+        stack.append(i)
+
+    return ''.join(stack)
+
+
+
+
