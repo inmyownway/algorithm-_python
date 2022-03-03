2fc742f4353efbaf283ecee2204a8f7c9280f427
jungmin <wjdals05070@gmail.com>
2022-03-03 15:34:11 +0900

    prgrms-ct-py8

--- Week2: Stack & Hash/방문 길이.py
+++ Week2: Stack & Hash/방문 길이.py
@@ -1,6 +1,23 @@
 def solution(dirs):
 
-    for i in len(dirs):
+    x,y = 0,0
+    check = dict()
 
+    for command in dirs:
+        if command == 'U' and y < 5:
+            check[(x,y,x,y+1)] = True
+            y += 1
+        elif command == 'D' and y > -5:
+            check[(x, y-1, x, y)] = True
+            y -= 1
+        elif command == 'L' and x > -5:
+            check[(x-1, y, x, y)] = True
+            x -= 1
+        elif command == 'R' and x < 5:
+            check[(x, y, x+1, y)] = True
+            x += 1
+    print(check)
+    return len(check)
 
-    return answer
+s="UDU"
+print(solution(s))
